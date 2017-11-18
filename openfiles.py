ef173e3c52221b253281e469a8c1b322fbe1755e
greatdevaks <greatdevaks@gmail.com>
2017-11-19 02:08:52 +0530

    Make copy of document
    Makes copy of current document in the git directory.

--- openfiles.py
+++ openfiles.py
@@ -1,6 +1,9 @@
 import psutil
+import shutil
 
 vec = psutil.pids()
+
+file_list = []
 
 for i in vec:
     if(psutil.Process(i).name() == "POWERPNT.EXE" or psutil.Process(i).name() == "WINWORD.EXE" or psutil.Process(i).name() == "Acrobat.exe"):
@@ -10,8 +13,19 @@ for i in vec:
                 res = str(x).strip("popenfile(path='")
                 res = res.strip("', fd=-1)")
                 print(res)
+                strippedRes = res.rsplit("\\", 1)[1]
+                shutil.copyfile(res, "C:\\Users\\Anmol-Sachdeva\\PycharmProjects\\Psutils\\" + strippedRes)
+                file_list.append(strippedRes)
             elif ("pdf" in str(x)):
                 res = str(x).strip("popenfile(path='")
                 res = res.strip("', fd=-1)") + "df"
+                strippedRes = res.rsplit("\\", 1)[1]
                 print(res)
+                shutil.copyfile(res, "C:\\Users\\Anmol-Sachdeva\\PycharmProjects\\Psutils\\" + strippedRes)
+                file_list.append(strippedRes)
+
+print(file_list)
+
+
+
 
